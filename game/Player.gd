dacb7696c47c8c0f82b93f49d5b8751ec5d85c1d
Poizenes <poizenes@errordev.de>
2020-02-03 19:26:38 +0100

    Fix rotation bugs
    * Rotation is now possible when not moving
    * Rotation doesn't reset anymore
    
    NOTE: Basic controls are done but feeling a little weird

--- game/Player.gd
+++ game/Player.gd
@@ -1,17 +1,15 @@
 extends KinematicBody2D
 
-
 var MAX_SPEED = 400
 var motion = Vector2.ZERO
 var controller = load("Controller.gd").new(Controller.Device.C0)
 
 func _physics_process(delta):
 	motion = Vector2.ZERO
-	var axis = controller.get_input_axis(0)
-	apply_movement(axis * delta)
+	apply_movement(controller.get_input_axis(0) * delta)
 	if controller.get_device() == Controller.Device.KEYBOARD:
 		look_at(get_global_mouse_position())
-	elif axis != Vector2.ZERO:
+	elif  controller.get_input_axis(1) != Vector2.ZERO:
 		rotation = controller.get_input_axis(1).angle()
 	motion = move_and_slide(motion)
 
