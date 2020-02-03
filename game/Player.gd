3511d66e73ac6b8cfff2c9b901b87c14cb59f49d
Poizenes <poizenes@errordev.de>
2020-02-03 19:09:34 +0100

    Override controls (updated version)
    * Added class 'Controller'
    * Removed function 'get_input_axis' from Player.gd
    * Added controls for keyboard and controller 0 (TODO: Add support for controllers 1 - 3)

--- game/Player.gd
+++ game/Player.gd
@@ -1,21 +1,19 @@
 extends KinematicBody2D
 
+
 var MAX_SPEED = 400
 var motion = Vector2.ZERO
+var controller = load("Controller.gd").new(Controller.Device.C0)
 
 func _physics_process(delta):
 	motion = Vector2.ZERO
-	var axis = get_input_axis()
+	var axis = controller.get_input_axis(0)
 	apply_movement(axis * delta)
+	if controller.get_device() == Controller.Device.KEYBOARD:
+		look_at(get_global_mouse_position())
+	elif axis != Vector2.ZERO:
+		rotation = controller.get_input_axis(1).angle()
 	motion = move_and_slide(motion)
-	rotation = get_input_axis().angle()
-	#look_at(get_global_mouse_position())
-
-func get_input_axis():
-	var axis = Vector2.ZERO
-	axis.x = Input.get_action_strength('ui_right') - Input.get_action_strength('ui_left')
-	axis.y = Input.get_action_strength('ui_down') - Input.get_action_strength('ui_up')
-	return axis.normalized()
 
 func apply_movement(acceleration):
 	motion += acceleration
