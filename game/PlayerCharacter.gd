e59c4be9f7d3fbab3647da91ace1271c63fa25a2
Poizenes <poizenes@errordev.de>
2020-03-05 19:48:56 +0100

    Create STAB ATTACK
    + Stab Attack
    + Player is added to "Player" group
    
    TODO: Hurt mechanic

--- game/PlayerCharacter.gd
+++ game/PlayerCharacter.gd
@@ -4,9 +4,20 @@ class_name PlayerCharacter
 var SPEED = 400
 var motion = Vector2.ZERO
 var controller
+var attack_ready
+var attack_timer
 
 func _init(device=Controller.Device.KEYBOARD):
 	controller = Controller.new(device)
+
+func _ready():
+	add_to_group("Player")
+	
+	attack_timer = Timer.new()
+	attack_timer.connect("timeout", self, "on_attack_timer_timeout")
+	attack_timer.set_wait_time(0.25)
+	add_child(attack_timer)
+	attack_ready = true
 
 func _physics_process(delta):
 	motion = Vector2.ZERO
@@ -16,7 +27,16 @@ func _physics_process(delta):
 	elif controller.get_input_axis(1) != Vector2.ZERO:
 		rotation = controller.get_input_axis(1).angle()
 	motion = move_and_slide(motion)
+	
+	if controller.is_just_pressed(Controller.Button.ATTACK) && attack_ready:
+		var sword = preload("res://Sword.tscn").instance()
+		add_child(sword)
+		attack_ready = false
+		attack_timer.start()
 
 func apply_movement(acceleration):
 	motion += acceleration
 	motion = motion.normalized() * SPEED
+
+func on_attack_timer_timeout():
+	attack_ready = true
