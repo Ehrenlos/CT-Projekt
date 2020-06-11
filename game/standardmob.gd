ea6d0633da4aa8c16feb22a570b6f364d8bc64fc
Poizenes <poizenes@errordev.de>
2020-06-11 16:55:34 +0200

    DAVID: CREATE STANDARD MOB

---
+++ game/standardmob.gd
@@ -0,0 +1,19 @@
+extends KinematicBody2D
+
+var SPEED = 200
+var pdirection = Vector2() 
+var pposition = Vector2()
+var dir = Vector2()
+
+func _ready():
+	pposition = get_parent().get_child(0).get_position()
+
+
+
+func _physics_process(delta):
+	pposition = get_parent().get_child(0).get_position()
+	dir = _give_dir()
+	move_and_slide(dir * SPEED)
+
+func _give_dir():
+	return (pposition - position).normalized()
